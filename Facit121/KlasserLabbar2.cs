060619c39ac976d320198ce0c1a8ef8d2842bf92
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-21 11:45:45 +0200

    2an fixad

--- Facit121/KlasserLabbar2.cs
+++ Facit121/KlasserLabbar2.cs
@@ -18,24 +18,37 @@ namespace Facit121
             Postnr = postnr;
             Postort = postort;
         }
+
+        public void Namnge(string nyttnamn)
+        {
+            Namn = nyttnamn;
+        }
+
         public DateTime BirthDate { get; set; }
         public string Namn { get; set; }
         public string Gatuadress { get; set; }
         public int Postnr { get; private set; }
         public string Postort { get; private set; }
+
+        public void FlyttaIhopTill(Person kerstin)
+        {
+            BytAddress(kerstin.Gatuadress,kerstin.Postnr,kerstin.Postort);         
+        }
     }
     public class KlasserLabbar2
     {
         public void Run()
         {
             var stefan = new Person(new DateTime(1972,8,3), "Stefan");
+            var kerstin = new Person(new DateTime(1973, 3, 5), "Kerstin");
 
-            stefan.Namn = "312123";
 
             //Meny
             //1. Byt address
             stefan.BytAddress("Testgatan 123",12233,"Teststad");
+            kerstin.BytAddress("Hejgatan 123", 11122, "Teststad");
 
+            stefan.FlyttaIhopTill(kerstin);
 
         }
     }
